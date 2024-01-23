2467290a17cfdd1e5bacc627c36b07371a817bf7
arepatiesa <carlosmarcos2004@gmail.com>
2024-01-22 22:00:12 -0400

    backend server

---
+++ index.js
@@ -0,0 +1,78 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+      },
+      {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+      },
+      {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+      }
+]
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes)
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post("/api/notes", (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+
+    res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(400).send('That page does not exist.')
+    }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
